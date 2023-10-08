81e7896c16d8fa745ff4f6551881f0f88cf6542b
Sean Boyer <seanboyer.sj@gmail.com>
2023-10-08 01:37:06 -0300

    Name our namespace

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controllers;
+namespace MuseUs\Controllers;
 
 use CodeIgniter\Controller;
 use CodeIgniter\HTTP\CLIRequest;
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Controllers;
+namespace MuseUs\Controllers;
 
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('home');
     }
 }
